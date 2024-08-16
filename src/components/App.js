e277ad05e714a0068dc3a4ef243280b064f2d1cd
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-08-17 03:17:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import './../styles/App.css';
-import CounterClick from './Counter';
+import '../styles/App.css';
+import CounterClick from "./Counter";
 const App = () => {
   return (
     <div>
