d927ac25893e65973ffbdb31674401f37da4b4d3
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-08-18 12:25:48 +0000

    1.3

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:8080',
     supportFile: 'cypress/support/e2e.js', // Adjust this if you don't have an e2e.js file
     specPattern: '**/*.spec.js',
   },
